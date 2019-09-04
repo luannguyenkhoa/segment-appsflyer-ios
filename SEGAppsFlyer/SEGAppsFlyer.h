c2d6fc35739b1b1fddc1c3d58e46d1425e047355
Luan Nguyen <luan.nguyenkhoa@asnet.com.vn>
2019-09-04 11:06:22 +0700

    Add support carthage

---
+++ SEGAppsFlyer/SEGAppsFlyer.h
@@ -0,0 +1,19 @@
+//
+//  SEGAppsFlyer.h
+//  SEGAppsFlyer
+//
+//  Created by Luan Nguyen on 9/4/19.
+//  Copyright © 2019 Luan Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SEGAppsFlyer.
+FOUNDATION_EXPORT double SEGAppsFlyerVersionNumber;
+
+//! Project version string for SEGAppsFlyer.
+FOUNDATION_EXPORT const unsigned char SEGAppsFlyerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SEGAppsFlyer/PublicHeader.h>
+
+
